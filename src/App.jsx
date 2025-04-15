5e221f74a8817ae0e4ba3c344dceee603eae43de
XLebowski <vbn-134@yandex.ru>
2025-04-15 14:04:46 +0300

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
 import Posts from './Components/Posts/Posts';
-import Users from './Components/Users/Users';
+import Clients from './Components/Clients/Clients';
 import Search from './Components/Search/Search';
-// import Header from './Components/Header';
 
 export default function App() {
     return (
@@ -13,7 +12,7 @@ export default function App() {
                 <Route path='/' element={<Layout />}>
                     {/* <Route index element={<Menu />} /> */}
                     <Route path='/posts' element={<Posts />} />
-                    <Route path='/users' element={<Users />} />
+                    <Route path='/users' element={<Clients />} />
                     <Route path='/search' element={<Search />} />
                 </Route>
             </Routes>
